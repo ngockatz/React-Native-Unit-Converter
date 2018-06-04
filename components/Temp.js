f802e1d69e02c492a2e37ac8e781640427e1452d
ngockatz <ngoc1260@gmail.com>
2018-06-04 16:39:31 -0400

    Made temp conversion look nicer

--- components/Temp.js
+++ components/Temp.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text, Picker, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class Temp extends Component {
 
     constructor(props){
     super(props);
-    this.state = { userInput: '', unitFrom: 'F', unitTo: 'C', result: 0 };
+    this.state = { userInput: '', unitFrom: 'F', unitTo: 'C', result: null };
     }
 
     calculate() {
-        let intermediary, target;
+        
         // Intermediary: C
+    if(this.state.userInput!==''){
+        let intermediary, target;
        if (this.state.unitFrom=='F'){
             intermediary = (this.state.userInput - 32) * 5 / 9;
        } 
@@ -25,73 +28,72 @@ export default class Temp extends Component {
             target = intermediary;
         };
 
-        target=target.toFixed(0)
+        target=target.toFixed(1)
         this.setState({result: target});
     }
-
+    else{
+        this.setState({'result': null})
+    }
+    }
     reverse(){
         let temp1,temp2;
         temp1=this.state.unitFrom;
         temp2=this.state.unitTo;
-        this.setState({unitFrom: temp2, unitTo:temp1});
-
+        this.setState({unitFrom: temp2, unitTo:temp1},()=>{
+        this.calculate()})
     }
     render() {
         return (
             <View>
                 <View style={styles.headerStyle} >
-                    <Text style={styles.headerText}> Temperature </Text>
+                    <Text style={styles.headerText}> Temperature/Nhiệt độ </Text>
                 </View>
 
                 <View style={styles.unitRow} >
 
-                    <View>
+                    <View style={{flex:2}}>
                         <View style={styles.unitSelection} >
                             <Picker
                                 selectedValue={this.state.unitFrom}
-                                style={{ height: 50, width: 100 }}
+                                style={{ height: 40, width: 100 }}
                                 onValueChange={(itemValue, itemIndex) => 
-                                    this.setState({ unitFrom: itemValue })
-                                    //this.calculate.bind(this)
+                                    this.setState({ unitFrom: itemValue },()=>{
+                                    this.calculate()})
                                 }>
                                 <Picker.Item label="°C" value="C" />
                                 <Picker.Item label="°F" value="F" />
                             </Picker>
                         </View>
                         <TextInput
+                            placeholder="(from)"
                             style={styles.inputStyle}
                             value={this.state.userInput}
                             onChangeText={text => 
-                                this.setState({ userInput: text })
-                                //this.calculate.bind(this)
+                                this.setState({ userInput: text },() => {
+                                this.calculate()})
                             }
-                            maxLength={10}
+                            maxLength={12}
                             keyboardType='numeric'
                         />
                     </View>
 
                     <TouchableOpacity 
-                        onPress={this.calculate.bind(this)} 
-                        style={{alignSelf:'center'}}
+                        onPress={this.reverse.bind(this)}
+                        style={{flex:1,paddingLeft:17,alignSelf:'center'}}
                     >
-                        <Text> Calculate </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity 
-                        onPress={this.reverse.bind(this)} 
-                        style={{alignSelf:'center'}}
-                    >
-                        <Text> Reverse </Text>
+                        <Icon name='swap-horizontal-variant' size={70} />
                     </TouchableOpacity>
 
 
-                    <View>
+                    <View style={{flex:2}}>
                         <View style={styles.unitSelection} >
                             <Picker
                                 style={styles.pickerStyle}
                                 selectedValue={this.state.unitTo}
-                                style={{ height: 50, width: 100 }}
+                                style={{ height: 40, width: 100 }}
                                 onValueChange={(itemVal, itemInd) => {
-                                    this.setState({ unitTo: itemVal }
+                                    this.setState({ unitTo: itemVal }, () =>
+                                    this.calculate()
                                     )}}>
                                 <Picker.Item label="°C" value="C" />
                                 <Picker.Item label="°F" value="F" />
@@ -112,12 +114,16 @@ export default class Temp extends Component {
 
 styles = {
     headerStyle: {
-        alignSelf: 'center',
         paddingBottom: 10,
+        backgroundColor: '#e82813',
+        alignSelf: 'stretch',
+        
+        
     },
     headerText:{
-        color:'red',
+        color:'#ffffff',
         fontSize: 20,
+        
         
     },
     unitRow: {
@@ -128,18 +134,19 @@ styles = {
     unitSelection: {
         flexDirection: 'row',
         justifyContent: 'space-around',
+        
     },
     resultText:{
         textAlign: 'center',
         fontWeight: 'bold',
         color: 'black',
-        fontSize: 15,
-        paddingTop: 15,
+        fontSize: 20,
+        paddingTop: 10,
     },
     inputStyle:{
         fontWeight: 'bold',
         color: 'black',
-        fontSize: 15,
+        fontSize: 20,
     },
     pickerStyle:{
         borderStyle: 'solid',
